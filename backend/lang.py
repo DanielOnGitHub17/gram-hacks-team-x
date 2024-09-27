b8ba2dbaf9f364a326b9c5db4898ce75374afb04
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-09-27 18:16:47 +0000

    added refs to templates

--- backend/lang.py
+++ backend/lang.py
@@ -1,15 +1,28 @@
 from flask import Flask
+from flask import url_for
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def home():
+    return render_template("home.html")
 
-@app.route('/practice')
-def index():
-    return 'Index Page'
+@app.route("/practice")
+def practice():
+    return render_template("practice.html")
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
+
+
+# with app.test_request_context():
+#     for route in ("home", "practice", "settings", "game"):
+#         print(url_for(route))
+    
